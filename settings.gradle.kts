0d7fa0ecddddf54c034eb8c603913429ff76e5cd
Deftu <me@deftu.dev>
2025-05-10 18:00:02 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,17 +2,27 @@ import groovy.lang.MissingPropertyException
 
 pluginManagement {
     repositories {
-        mavenLocal()
+        // Snapshots
+        maven("https://maven.deftu.dev/snapshots")
+        maven("https://s01.oss.sonatype.org/content/groups/public/")
+
+        // Repositories
+        maven("https://maven.deftu.dev/releases")
+        maven("https://maven.fabricmc.net")
+        maven("https://maven.architectury.dev/")
+        maven("https://maven.minecraftforge.net")
+        maven("https://repo.essential.gg/repository/maven-public")
+        maven("https://jitpack.io/")
+
+        // Default repositories
         gradlePluginPortal()
         mavenCentral()
-        maven("https://maven.deftu.dev/releases/")
-        maven("https://maven.deftu.dev/snapshots/")
-        maven("https://jitpack.io/")
-        maven("https://maven.fabricmc.net/")
-        maven("https://maven.minecraftforge.net/")
-        maven("https://maven.architectury.dev/")
-        maven("https://repo.essential.gg/repository/maven-public/")
+        mavenLocal()
     }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.8.+")
 }
 
 rootProject.name = extra["project.name"]?.toString() ?: throw MissingPropertyException("The project name was not configured!")
