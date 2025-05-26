74344513a93ec47cb8a16735c23ade9719723df9
Deftu <me@deftu.dev>
2025-05-26 16:31:46 +0200

    Add extension function to toggle mutable boolean state

--- src/commonMain/kotlin/dev/deftu/stateful/utils/booleans.kt
+++ src/commonMain/kotlin/dev/deftu/stateful/utils/booleans.kt
@@ -1,6 +1,11 @@
 package dev.deftu.stateful.utils
 
+import dev.deftu.stateful.MutableState
 import dev.deftu.stateful.State
+
+public fun MutableState<Boolean>.toggle() {
+    set { !it }
+}
 
 public infix fun State<Boolean>.and(other: State<Boolean>): State<Boolean> {
     return mappedStateOf(zippedStateOf(this, other)) { (a, b) -> a && b }
