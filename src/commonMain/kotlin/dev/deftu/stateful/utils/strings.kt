b1d32eeb352184d362ac0decad00a8f969301d67
Deftu <me@deftu.dev>
2025-05-26 16:32:44 +0200

    Add trim, lowercase and uppercase string state extensions

--- src/commonMain/kotlin/dev/deftu/stateful/utils/strings.kt
+++ src/commonMain/kotlin/dev/deftu/stateful/utils/strings.kt
@@ -3,7 +3,7 @@ package dev.deftu.stateful.utils
 import dev.deftu.stateful.State
 
 public val State<String>.length: State<Int>
-    get() = mappedStateOf(this) { value -> value.length }
+    get() = mappedStateOf(this, String::length)
 
 public fun State<String>.contains(other: String, ignoreCase: Boolean = false): State<Boolean> {
     return mappedStateOf(this) { value -> value.contains(other, ignoreCase) }
@@ -30,17 +30,29 @@ public fun State<String>.endsWith(other: State<String>, ignoreCase: Boolean = fa
 }
 
 public fun State<String>.isEmpty(): State<Boolean> {
-    return mappedStateOf(this) { value -> value.isEmpty() }
+    return mappedStateOf(this, String::isEmpty)
 }
 
 public fun State<String>.isNotEmpty(): State<Boolean> {
-    return mappedStateOf(this) { value -> value.isNotEmpty() }
+    return mappedStateOf(this, String::isNotEmpty)
 }
 
 public fun State<String>.isBlank(): State<Boolean> {
-    return mappedStateOf(this) { value -> value.isBlank() }
+    return mappedStateOf(this, String::isBlank)
 }
 
 public fun State<String>.isNotBlank(): State<Boolean> {
-    return mappedStateOf(this) { value -> value.isNotBlank() }
+    return mappedStateOf(this, String::isNotBlank)
+}
+
+public fun State<String>.trim(): State<String> {
+    return mappedStateOf(this, String::trim)
+}
+
+public fun State<String>.lowercase(): State<String> {
+    return mappedStateOf(this, String::lowercase)
+}
+
+public fun State<String>.uppercase(): State<String> {
+    return mappedStateOf(this, String::uppercase)
 }
